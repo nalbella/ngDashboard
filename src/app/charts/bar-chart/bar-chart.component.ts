89c8e1f463bcc7449b20ba2ac15dbc42706bf7e9
Ignacio Albella <nalbella@gmail.com>
2021-02-02 18:11:21 +0000

    implement pagination, bar chart component

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { TripsDataService } from '../../services/trips-data.service';
+import * as moment from 'moment';
 
-const SAMPLE_BARCHART_DATA: any[] = [
-  { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Trips'},
-  { data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Trips'}
-]
+// const SAMPLE_BARCHART_DATA: any[] = [
+//   { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Trips'},
+//   { data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Trips'}
+// ]
 
-const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7']; 
+// const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7']; 
 
 @Component({
   selector: 'app-bar-chart',
@@ -14,10 +16,14 @@ const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W
 })
 export class BarChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _tripsDataService: TripsDataService) { }
+  
+  trips: any;
+  tripLabels: string[];
+  tripData: number[];
 
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  public barChartData: any[];
+  public barChartLabels: string[];
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartOptions: any = {
@@ -25,7 +31,49 @@ export class BarChartComponent implements OnInit {
     responsive: true
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this._tripsDataService.getTrips(1, 100)
+      .subscribe(res => {
+        //console.log(res);
+        //console.log(res['page']['data']);
+        const localChartData = this.getChartData(res);
+        this.barChartLabels = localChartData.map((x: any) => x[0]).reverse();
+        this.barChartData = [{ 'data': localChartData.map((x: any) => x[1]), 'label': 'Price Amount'}];
+      });
   }
+
+  getChartData(res: Response) {
+    this.trips = res['page']['data'];
+    const data = this.trips.map((o: any)  => o.quotedPrice);
+    const labels = this.trips.map((o: any) => moment(new Date(o.createdDate)).format('YY-MM-DD'));
+
+    const formattedTrips = this.trips.reduce((r: any, e:any) => {
+      r.push([moment(e.createdDate).format('YY-MM-DD'), e.quotedPrice]);
+      return r;
+    }, []);
+    
+    //console.log(formattedTrips);
+
+    const p = [];
+
+    const chartData = formattedTrips.reduce((r: any, e:any) => {
+      const key = e[0];
+      if (!p[key]) {
+        p[key] = e;
+        r.push(p[key]);
+      } else {
+        p[key][1] += e[1];
+      }
+      return r;
+    }, []);
+
+    return chartData;
+
+
+    //const labels = this.trips.map((o: any) => new Date(o.rta));
+    //console.log(labels);
+  }
+
+
 
 }
--- src/app/services/trips-data.service.ts
+++ src/app/services/trips-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class TripsDataService {
@@ -8,7 +8,9 @@ export class TripsDataService {
     constructor(private _http: HttpClient) { }
 
     getTrips(pageIndex: number, pageSize: number) {
-        return this._http.get('http://localhost:5000/api/trip/' + pageIndex + '/' + pageSize)
-        .map(res => res );
+        return this._http.get<Response>('http://localhost:5000/api/trip/' + pageIndex + '/' + pageSize)
+        //.pipe(map(res => res.results || []));
+        //.subscribe(response => console.log(response));
+        .pipe(map(res => res));
     }
 }
